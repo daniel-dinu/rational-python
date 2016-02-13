425e8968407d9f8ae6b153815adc7cb77818c01e
Daniel Dinu <dinu.dumitrudaniel@yahoo.com>
2016-02-13 01:19:36 +0100

    Check if it fails

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,13 +3,16 @@
 set -e
 set -x
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Initialize pyenv
-    PYENV_ROOT="$HOME/.pyenv"
-    PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-fi
+#if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+#    # Initialize pyenv
+#    PYENV_ROOT="$HOME/.pyenv"
+#    PATH="$PYENV_ROOT/bin:$PATH"
+#    eval "$(pyenv init -)"
+#fi
 
+# Activate virtual environment
 source ~/.venv/bin/activate
+
+python --version
 
 python setup.py test
